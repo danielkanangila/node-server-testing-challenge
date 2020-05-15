c0c0b0dc32c3306d536b57045b339cb9ea7e7432
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-05-14 20:58:32 -0400

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -33,9 +33,10 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      host: process.env.DATABASE_HOST,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
     },
     pool: {
       min: 2,
